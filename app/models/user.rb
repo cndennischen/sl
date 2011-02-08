adfa1bc56cf4b99a6b0da6b75e044567d2c16b73
Heigh Tech LLC <heigh-tech@heigh-tech.com>
2011-02-07 19:32:46 -0500

    initialize sketches to an empty string when user is created

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
       user.provider = auth["provider"]
       user.uid = auth["uid"]
       user.name = auth["user_info"]["name"]
+      user.sketches = ""
     end
   end
 end
