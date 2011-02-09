a02e416235d1d305517dd48369666ae6f53a8687
Heigh-Tech <heigh-tech@heigh-tech.com>
2011-02-09 10:09:36 -0500

    set sketch's user id on creation

--- app/models/sketch.rb
+++ app/models/sketch.rb
@@ -5,6 +5,7 @@ class Sketch < ActiveRecord::Base
     create! do |sketch|
       sketch.name = "Untitled Sketch"
       sketch.content = "{}"
+			sketch.user_id = current_user.id
     end
   end
 end
