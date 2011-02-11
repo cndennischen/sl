2719eb0a3f7c762fc9eb2c73f1df82c6e6e19d48
Heigh-Tech <heigh-tech@heigh-tech.com>
2011-02-11 08:16:01 -0500

    get access level from chrome webstore licensing api

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def access?
+  def access_level
     # send a request to the chrome webstore licensing api
     appId = "delppejinhhpcmimgfchjkbkpanhjkdj"
     userId = CGI::escape(current_user.uid)
@@ -7,13 +7,13 @@ module ApplicationHelper
       :client_credential_key => 'anonymous',
       :client_credential_secret => 'anonymous',
       :token_credential_key => OAUTH_CONFIG['token_credential_key'],
-      :token_credential_secret => OAUTH_CONFIG['token_credential_key']
+      :token_credential_secret => OAUTH_CONFIG['token_credential_secret']
     )
     response = client.fetch_protected_resource(
       :uri => "https://www.googleapis.com/chromewebstore/v1/licenses/#{appId}/#{userId}"
     )
     # get the accessLevel from the json response
-    JSON.parse(response[2][0])["result"]
+    return JSON.parse(response[2][0])["accessLevel"]
   end
 
 end
---
+++ config/initializers/load_config.rb
@@ -0,0 +1,2 @@
+OAUTH_CONFIG = YAML.load_file("#{RAILS_ROOT}/config/oauth.yml")
+
