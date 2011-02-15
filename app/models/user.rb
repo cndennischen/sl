7cd9733f23d1d611e72f40ffffc916fe5c79f411
Heigh-Tech <heigh-tech@heigh-tech.com>
2011-02-14 20:14:36 -0500

    removed paypal ipn, going to research other solutions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,14 @@
 class User < ActiveRecord::Base
   has_many :sketches, :dependent => :destroy
-  has_many :ipns
   
-  validates_presence_of :provider, :uid, :name, :email
-  validates_uniqueness_of :uid, :email
+  validates_presence_of :provider, :uid, :name
+  validates_uniqueness_of :uid
   
   def self.create_with_omniauth(auth)
     create! do |user|
       user.provider = auth["provider"]
       user.uid = auth["uid"]
       user.name = auth["user_info"]["name"]
-      user.email = auth["user_info"]["email"]
     end
   end
 end
