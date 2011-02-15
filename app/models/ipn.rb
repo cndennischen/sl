edb95bd21cec7e939068379bcb5614b97ffb9bb8
Heigh-Tech <heigh-tech@heigh-tech.com>
2011-02-15 13:32:53 -0500

    saves user's email after ipn

--- app/models/ipn.rb
+++ app/models/ipn.rb
@@ -1,5 +1,6 @@
 class Ipn < ActiveRecord::Base
   attr_accessible :params, :user_id, :status, :transaction_id
+  validates_presence_of :user_id
   belongs_to :user
   serialize :params
   after_create :process_ipn
@@ -7,7 +8,9 @@ class Ipn < ActiveRecord::Base
   private
   
   def process_ipn
-    
+    # set the user's email address
+    user.email = params[:payer_email]
+    user.save
   end
   
 end
