e190a6f76a6453521850ea9999157b17b8686dc3
Heigh-Tech <heigh-tech@heigh-tech.com>
2011-02-16 20:34:52 -0500

    changed name from SimpleSketch to Sketch Lab

--- app/mailers/notifications.rb
+++ app/mailers/notifications.rb
@@ -1,6 +1,6 @@
 class Notifications < ActionMailer::Base
   def invalid_ipn(ipn)
     @ipn = ipn
-    mail(:to => MAIL_CONFIG[:admin_email], :from => MAIL_CONFIG[:admin_email], :subject => "SimpleSketch: Invalid IPN")
+    mail(:to => MAIL_CONFIG[:admin_email], :from => MAIL_CONFIG[:admin_email], :subject => "Sketch Lab: Invalid IPN")
   end
 end
