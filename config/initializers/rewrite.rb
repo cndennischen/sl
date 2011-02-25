73f521b30ac6724ccaba0e5183bdc7fee1d1798e
Heigh-Tech <heigh-tech@heigh-tech.com>
2011-02-25 13:18:28 -0500

    remove comment

--- config/initializers/rewrite.rb
+++ config/initializers/rewrite.rb
@@ -1,8 +1,3 @@
-=begin
-config.middleware.insert_before(Rack::Lock, Rack::Rewrite) do
- r301 %r{^/(.*)/$}, '/$1'
-end
-=end
 Rails.application.config.middleware.use Rack::Rewrite do
   r301 %r{^/(.*)/$}, '/$1'
 end
