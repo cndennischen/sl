f8c38fcaebc5e35d7a3558f4c60167db06fe389d
Heigh-Tech <heigh-tech@heigh-tech.com>
2011-02-26 20:57:39 -0500

    export enhancements

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,3 +1,6 @@
 PDFKit.configure do |config|
   config.wkhtmltopdf = Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s if RAILS_ENV == 'production'
+  config.default_options = {
+    :print_media_type => true
+  }
 end
