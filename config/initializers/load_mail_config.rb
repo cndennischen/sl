e2a745661193908e45ad804d29353905e08eddb1
Heigh-Tech <heigh-tech@heigh-tech.com>
2011-03-01 11:52:05 -0500

    remove mailer

--- config/initializers/load_mail_config.rb
+++
@@ -1,2 +0,0 @@
-raw_config = File.read("#{Rails.root}/config/mail_config.yml")
-MAIL_CONFIG = YAML.load(raw_config).symbolize_keys
--- lib/development_mail_interceptor.rb
+++
@@ -1,7 +0,0 @@
-# This class intercepts all outgoing email in development mode and sends it to the admin's email address.
-class DevelopmentMailInterceptor
-  def self.delivering_email(message)
-    message.subject = "#{message.to} #{message.subject}"
-    message.to = MAIL_CONFIG[:admin_email]
-  end
-end
