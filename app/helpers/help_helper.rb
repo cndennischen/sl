9e37660dc17e1485f1a764a5539e15327dff01bb
Heigh Tech LLC <heigh-tech@heigh-tech.com>
2011-03-04 17:19:00 +0000

    skeleton code for faq

--- app/helpers/help_helper.rb
+++ app/helpers/help_helper.rb
@@ -1,2 +1,9 @@
 module HelpHelper
+  def faq(q, a)
+    # generate html code for an FAQ question and expandable answer
+    html = ''
+    html += '<a href="#">' + q + '</a>'
+    html += '<p>' + a + '</p>'
+    html
+  end
 end
