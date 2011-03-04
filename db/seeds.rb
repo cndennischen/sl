7f57a7b8ce3150c106d39b932594fc4b11dc2684
Heigh-Tech <heigh-tech@heigh-tech.com>
2011-03-04 17:01:48 -0500

    add faq

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,10 @@
 
 # Create the FAQ questions
 Faq.create(:question => 'Can I upgrade and downgrade my account at any time',
-           :answer => 'Of course you can! Just go to the <a href="/account">account</a> page.');
+           :answer => 'Of course you can! Just go to the <a href="/account">account</a> page.')
            
 Faq.create(:question => 'Do you offer free student accounts',
-           :answer => 'We sure do. Please <a href="http://sketchlab.mojohelpdesk.com/mytickets/create">submit a support ticket</a> to request one.');
+           :answer => 'We sure do. Please <a href="http://sketchlab.mojohelpdesk.com/mytickets/create">submit a support ticket</a> to request one.')
+
+Faq.create(:question => 'Can I cancel my account',
+           :answer => 'You can cancel your account by going to the <a href="/account">account</a> page and clicking "Delete Account". If you are on the paid plan, you will need to downgrade to the free plan first.')
