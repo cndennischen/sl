85cec80cdf06d6ff47ee5511082178795e0d1dd3
Heigh Tech LLC <heigh-tech@heigh-tech.com>
2011-03-04 19:08:47 +0000

    remove line break

--- app/helpers/help_helper.rb
+++ app/helpers/help_helper.rb
@@ -2,7 +2,7 @@ module HelpHelper
   def faq(q, a)
     # generate html code for an FAQ question and expandable answer
     html = '<a class="question" href="#">' + q + '?</a>'
-    html += '<p>' + a + '</p><br />'
+    html += '<p>' + a + '</p>'
     html.html_safe
   end
 end
