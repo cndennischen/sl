138d2b6f38e20e11825090e9592e6f0ee0f4844e
Heigh-Tech <heigh-tech@heigh-tech.com>
2011-03-04 13:23:24 -0500

    fix up html

--- app/helpers/help_helper.rb
+++ app/helpers/help_helper.rb
@@ -1,9 +1,8 @@
 module HelpHelper
   def faq(q, a)
     # generate html code for an FAQ question and expandable answer
-    html = ''
     html += '<a class="question" href="#">' + q + '?</a>'
-    html += '<p>' + a + '</p><br />'
+    html += '<p>' + a + '</p>'
     html.html_safe
   end
 end
