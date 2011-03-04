7920f1e20880c928b5af4f53820085b466ed00ea
Heigh Tech LLC <heigh-tech@heigh-tech.com>
2011-03-04 19:36:55 +0000

    move init code to separate function

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,8 @@
-$(function() {
+$(init);
+
+function init() {
   //fade out flashes
   setTimeout(function() {
     $("#flash_notice, #flash_error, #flash_alert").fadeOut(3000);
   }, 5000);
-});
+}
