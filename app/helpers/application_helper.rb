b9b98c80bc7bdc28f63fb7ac5234a57ea9f3d342
Heigh Tech LLC <heigh-tech@heigh-tech.com>
2011-03-06 16:16:42 +0000

    add newsletter subscribe helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,5 +2,9 @@ module ApplicationHelper
   def title(page_title)
     @title = page_title
   end
+  
+  def newsletter_subscribe
+    '<a href="http://eepurl.com/cSPjA">Subscribe to our newsletter</a>'.html_safe
+  end
 
 end
