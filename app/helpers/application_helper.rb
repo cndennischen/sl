c55815059e065a91abbe39f711804581db7eef9d
Heigh Tech LLC <heigh-tech@heigh-tech.com>
2011-03-06 16:35:41 +0000

    Revert "add newsletter subscribe helper method"
    This reverts commit b9b98c80bc7bdc28f63fb7ac5234a57ea9f3d342.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,5 @@ module ApplicationHelper
   def title(page_title)
     @title = page_title
   end
-  
-  def newsletter_subscribe
-    '<a href="http://eepurl.com/cSPjA">Subscribe to our newsletter</a>'.html_safe
-  end
 
 end
