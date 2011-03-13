fb6d6147609523b28e3780718959a02b3584684f
Heigh-Tech <heigh-tech@heigh-tech.com>
2011-03-13 07:59:19 -0400

    different flash notice for sign up

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,15 @@
 class SessionsController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
-    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"])
+    if user
+      flash[:notice] = "Signed In!"
+    else
+      user = User.create_with_omniauth(auth)
+      flash[:notice] = "Signed Up!"
+    end
     session[:user_id] = user.id
-    redirect_to root_url, :notice => "Signed in!"
+    redirect_to root_url
   end
 
   def destroy
