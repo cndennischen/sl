e72a9828f22ba353bbf9551bba942ed3726e4ae9
Heigh Tech LLC <heigh-tech@heigh-tech.com>
2011-03-16 15:00:30 -0400

    selenium tests

--- test/unit/sketch_test.rb
+++ test/unit/sketch_test.rb
@@ -6,18 +6,44 @@ class SketchTest < ActiveSupport::TestCase
     assert s.save
   end
   
-  test "create faq without name" do
+  test "create sketch without name" do
     s = Factory.build(:sketch, :name => nil)
     assert !s.save
   end
   
-  test "create faq without content" do
+  test "create sketch without content" do
     s = Factory.build(:sketch, :content => nil)
     assert s.save
   end
   
-  test "create faq without user_id" do
+  test "create sketch without user_id" do
     s = Factory.build(:sketch, :user_id => nil)
     assert !s.save
+  end
+  
+  test "create sketch from browser" do
+    setup_selenium
+    signin
+    
+    # test creating a new sketch
+    @selenium.open "/"
+    @selenium.type "name", "Test Sketch"
+    @selenium.click "commit"
+    @selenium.wait_for_page_to_load "30000"
+    # test very basic sketch editing functionality
+    @selenium.click "addBtn"
+    begin
+        assert @selenium.is_text_present("Window")
+    rescue Test::Unit::AssertionFailedError
+        @verification_errors << $!
+    end
+    @selenium.click "undoBtn"
+    begin
+        assert !@selenium.is_text_present("Window")
+    rescue Test::Unit::AssertionFailedError
+        @verification_errors << $!
+    end
+    
+    teardown_selenium
   end
 end
