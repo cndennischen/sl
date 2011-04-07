0f2c2ff62ceb38ba0c924da1340beb94b2beb766
Heigh-Tech <heigh-tech@heigh-tech.com>
2011-04-07 17:41:50 -0400

    just check for the element, not the text

--- spec/requests/sketch_spec.rb
+++ spec/requests/sketch_spec.rb
@@ -32,7 +32,7 @@ describe 'Sketches' do
     post_via_redirect '/new', :name => 'Test Sketch 2'
     current_path.should == '/'
     Sketch.count.should == 1
-    page.should have_content('You cannot have more than one sketch on the free plan. Upgrade to the paid plan to have unlimited sketches.')
+    page.should have_selector('#flash_error')
     page.should have_no_content('Test Sketch 2')
   end
 end
