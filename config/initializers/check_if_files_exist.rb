7ca0ce2c4d3de548d2b591acdea6688d5e220f9a
Heigh-Tech <heigh-tech@heigh-tech.com>
2011-04-24 08:41:25 -0400

    improve error message

--- config/initializers/check_if_files_exist.rb
+++ config/initializers/check_if_files_exist.rb
@@ -1,5 +1,5 @@
 # Make sure the necessary configuration files are present
 unless File.exists?('config/app_config.yml') && File.exists?('config/initializers/secret_token.rb')
   # Remind the user to create config files
-  raise 'Configuration files missing!'
+  raise 'Sketch Lab configuration files missing!'
 end
