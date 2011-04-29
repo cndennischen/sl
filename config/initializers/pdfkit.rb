9ba5f2c947a715c93550040bcc66ecbe56b3d1a1
Heigh Tech LLC <heigh-tech@heigh-tech.com>
2011-04-28 20:52:14 -0400

    configure PDFKit / WKHTMLTOPDF for Heroku

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -1,7 +1,7 @@
 # Set up PDFKit for exporting sketches
 PDFKit.configure do |config|
   # Set the path to PDFKit, adjust depending on host
-  config.wkhtmltopdf = '/usr/local/bin/wkhtmltopdf'
+  config.wkhtmltopdf = Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s if Rails.env == 'production'
   config.default_options = {
     :print_media_type => true, # So that css can detect it
     :encoding => 'utf-8' # So check marks come out ok
