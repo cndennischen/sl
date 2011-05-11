7a1499dd2318a71c21ff4478c560902dc41d20cf
Heigh Tech LLC <heigh-tech@heigh-tech.com>
2011-05-11 10:15:37 -0400

    sign up button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,7 @@ module ApplicationHelper
     @title = page_title
   end
 
+  # Creates a tab for tabbed navigation
   def tab(name, path)
     if current_page?(path)
       html = '<span class="tab">' + name + '</span>'
@@ -13,4 +14,11 @@ module ApplicationHelper
     html.html_safe
   end
 
+  # Shows a signup button
+  def signup_button
+    # Only show signup button if there is no user signed in
+    unless current_user
+      view_context.link_to(image_tag('signup.png'), '/signin', :class => 'signup_button')
+    end
+  end
 end
