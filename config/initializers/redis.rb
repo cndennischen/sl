c64b30db4f6abe957d1455d28cef744b980354bd
Heigh Tech LLC <heigh-tech@heigh-tech.com>
2011-05-17 20:43:41 -0400

    fix deploy time not getting set

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,4 +2,6 @@ if ENV['REDISTOGO_URL']
   # Set up Redis
   uri = URI.parse(ENV['REDISTOGO_URL'])
   REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+  # Update last deployed time (we're assuming the app is (re)starting because something changed)
+  REDIS.set('last_deployed', Time.now.getutc)
 end
--- lib/tasks/deploy.rake
+++
@@ -1,8 +0,0 @@
-desc 'Update the "last deployed" time'
-task :update_last_deployed do
-  if defined?(REDIS)
-    REDIS.set('last_deployed', Time.now.getutc)
-  else
-    puts 'Redis not set up'
-  end
-end
